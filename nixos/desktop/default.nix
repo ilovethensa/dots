dfbca3137d665afa2ab0fc824481b888f78fa60b
Bill Gates <me@theholytachanka.com>
2024-03-14 18:56:27 +0000

    Added some env variables

--- nixos/desktop/default.nix
+++ nixos/desktop/default.nix
@@ -78,6 +78,17 @@
   boot.kernelParams = [ "mitigations=off" ];
   hardware.bluetooth.enable = false;
   programs.hyprland.enable = true;
+  system.environment = {
+    GDK_BACKEND = "wayland,x11";
+    QT_QPA_PLATFORM = "wayland;xcb";
+    SDL_VIDEODRIVER = "wayland";
+    CLUTTER_BACKEND = "wayland";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    QT_AUTO_SCREEN_SCALE_FACTOR = 1;
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
