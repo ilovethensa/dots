09d22c6a8380a596b94d2aeb18c758a84f96132c
Bill Gates <me@theholytachanka.com>
2024-03-15 20:22:25 +0000

    fix

--- flake.nix
+++ flake.nix
@@ -2,35 +2,88 @@
   description = "Your new nix config";
 
   inputs = {
+    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     impermanence.url = "github:nix-community/impermanence";
+
+    # Home manager
     home-manager = {
-      url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+      url =
+        "https://github.com/nix-community/home-manager/archive/master.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    #nix-on-droid = {
+    #  url = "github:nix-community/nix-on-droid/release-23.11";
+    #  inputs.nixpkgs.follows = "nixpkgs";
+    #};
+
+    # TODO: Add any other flake you might need
+    # hardware.url = "github:nixos/nixos-hardware";
+
+    # Shameless plug: looking for a way to nixify your themes and make
+    # everything match nicely? Try nix-colors!
+    # nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = { self, nixpkgs, home-manager, chaotic, firefox-addons, impermanence, ... }@inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , home-manager
+    , chaotic
+    , firefox-addons
+    , impermanence
+    , ...
+    }@inputs:
     let
       inherit (self) outputs;
-      systems = [ "aarch64-linux" "i686-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      # Supported systems for your flake packages, shell, etc.
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      # This is a function that generates an attribute by calling a function you
+      # pass to it, with each system as an argument
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+      # Your custom packages
+      # Accessible through 'nix build', 'nix shell', etc
+      packages =
+        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      # Formatter for your nix files, available through 'nix fmt'
+      # Other options beside 'alejandra' include 'nixpkgs-fmt'
+      formatter =
+        forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+      # Your custom packages and modifications, exported as overlays
       overlays = import ./overlays { inherit inputs; };
+      # Reusable nixos modules you might want to export
+      # These are usually stuff you would upstream into nixpkgs
       nixosModules = import ./modules/nixos;
+      # Reusable home-manager modules you might want to export
+      # These are usually stuff you would upstream into home-manager
       homeManagerModules = import ./modules/home-manager;
+
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
       nixosConfigurations = {
+        # FIXME replace with your hostname
         desktop = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           modules = [
+            # > Our main nixos configuration file <
             chaotic.nixosModules.default
             ./nixos/desktop
           ];
@@ -38,6 +91,7 @@
         thinkpad = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           modules = [
+            # > Our main nixos configuration file <
             chaotic.nixosModules.default
             ./nixos/thinkpad
           ];
@@ -45,11 +99,17 @@
         server = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           modules = [
+            # > Our main nixos configuration file <
             chaotic.nixosModules.default
             impermanence.nixosModules.impermanence
+
             ./nixos/server
           ];
         };
+        #nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+        #  modules = [ ./nixos/pixel ];
+        #};
+
       };
     };
 }
