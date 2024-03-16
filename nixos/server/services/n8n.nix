422d1262bd62a1ffa6d2002957fc79d2e4851203
Bill Gates <me@theholytachanka.com>
2024-03-16 12:18:31 +0000

    updated n8n

--- nixos/server/services/n8n.nix
+++ nixos/server/services/n8n.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, ... }:
 {
   virtualisation.oci-containers.containers."n8n" = {
-    image = "docker://docker.n8n.io/n8nio/n8n";
+    image = "docker://docker.n8n.io/n8nio/n8n:latest";
     ports = [ "5678:5678" ];
     volumes = [ "/srv/AppData/n8n:/home/node/.n8n" ];
     environment = { WEBHOOK_URL = "https://n8n.theholytachanka.com"; };
