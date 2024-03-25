2c68a6b6e3efbcfcf2ed6ab60d11002ece155c8b
Bill Gates <me@theholytachanka.com>
2024-03-25 12:19:33 +0000

    stuff

--- nixos/server/services/playit.nix
+++ nixos/server/services/playit.nix
@@ -5,7 +5,7 @@
     description = "Playit.gg daemon";
     serviceConfig = {
       Type = "forking";
-      ExecStart = "/home/tht/Mindustry/playit-linux-amd64 run";
+      ExecStart = "/home/tht/Mindustry/playit-linux-amd64 --secret_path /home/tht/.config/playit_gg/playit.toml";
       ExecStop = "pkill playit-linux-amd64";
       Restart = "on-failure";
     };
