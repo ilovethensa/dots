e2e0cc6f73463ab9eb35e4f2c51de64bcb8778d5
Bill Gates <me@theholytachanka.com>
2024-04-15 19:48:26 +0300

    fix

--- nixos/common/amd.nix
+++ nixos/common/amd.nix
@@ -11,7 +11,9 @@
   # Some programs hard-code the path to HIP
   systemd.tmpfiles.rules = ["L+ /opt/rocm/hip - - - - ${pkgs.rocmPackages.clr}"];
   # Enable ROCM on my RX 580
-  environment.variables = {ROC_ENABLE_PRE_VEGA = "1";};
+  environment.variables = {
+    ROC_ENABLE_PRE_VEGA = "1";
+  };
 
   # Additional hardware configurations
   hardware = {
