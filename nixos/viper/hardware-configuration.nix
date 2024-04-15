b891ef7a2f3d50195e887419dcfad5bde373327a
Bill Gates <me@theholytachanka.com>
2024-04-15 19:54:48 +0300

    merged

--- nixos/viper/hardware-configuration.nix
+++ nixos/viper/hardware-configuration.nix
@@ -11,37 +11,37 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
-
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
-  boot.extraModulePackages = [];
-
-  fileSystems."/" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = ["defaults" "size=2G" "mode=755"];
+  boot = {
+    initrd = {
+      availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
+      kernelModules = [];
+      luks.devices."cryptroot".device = "/dev/disk/by-uuid/40199b7d-67de-44db-bcbd-0a3ab72627b6";
+    };
+    kernelModules = ["kvm-amd"];
+    extraModulePackages = [];
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/9F58-71A4";
-    fsType = "vfat";
+  fileSystems = {
+    "/" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = ["defaults" "size=2G" "mode=755"];
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/9F58-71A4";
+      fsType = "vfat";
+    };
+    "/home" = {
+      device = "/dev/disk/by-uuid/e097e53b-d283-4c25-aa04-452f3b376349";
+      fsType = "btrfs";
+      options = ["subvol=@home" "noatime" "compress=zstd" "ssd"];
+    };
+    "/nix" = {
+      device = "/dev/disk/by-uuid/e097e53b-d283-4c25-aa04-452f3b376349";
+      fsType = "btrfs";
+      options = ["subvol=@nix" "noatime" "compress=zstd" "ssd"];
+    };
   };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/e097e53b-d283-4c25-aa04-452f3b376349";
-    fsType = "btrfs";
-    options = ["subvol=@home" "noatime" "compress=zstd" "ssd"];
-  };
-
-  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/40199b7d-67de-44db-bcbd-0a3ab72627b6";
-
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/e097e53b-d283-4c25-aa04-452f3b376349";
-    fsType = "btrfs";
-    options = ["subvol=@nix" "noatime" "compress=zstd" "ssd"];
-  };
-
   swapDevices = [];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
