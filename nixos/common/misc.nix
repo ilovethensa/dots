ace52efc0c0b3bdbf326137c096ea858c1cb765b
Bill Gates <me@theholytachanka.com>
2024-04-15 20:08:54 +0300

    added appimage support

--- nixos/common/misc.nix
+++ nixos/common/misc.nix
@@ -57,4 +57,13 @@
       value.source = value.flake;
     })
     config.nix.registry;
+  # Enable appimage support
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
+    magicOrExtension = "\\x7fELF....AI\\x02";
+  };
 }
