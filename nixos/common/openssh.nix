020afd5ca5c6813e9b96ddc42bb85b2f1704d824
Bill Gates <me@theholytachanka.com>
2024-04-21 19:50:13 +0300

    updated banner

--- nixos/common/openssh.nix
+++ nixos/common/openssh.nix
@@ -12,15 +12,15 @@
       69 # Funny number
     ];
     banner = ''
-      **********************************************************************
-      **                      Official CIA spyware node                   **
-      **                                                                  **
-      ** This system can only by accessed by the SSH backdoor our friends **
-      ** at the NSA gave us, no other way is possible. If you dont have   **
-      ** Please contact your authoritives                                 **
-      **                                                                  **
-      ** All activities performed on this system are logged and monitored.**
-      **********************************************************************
+      ----------------------------------------------------------------------
+      |                      Official CIA spyware node                     |
+      |                                                                    |
+      |  This system can only by accessed by the SSH backdoor our friends  |
+      |  at the NSA gave us, no other way is possible. If you dont have it |
+      |  Please contact your authoritives                                  |
+      |                                                                    |
+      |  All activities performed on this system are logged and monitored. |
+      ----------------------------------------------------------------------
     '';
     # require public key authentication for better security
     settings = {
