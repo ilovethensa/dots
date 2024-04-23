59ed9631023f99c1b1e05b4f68002ede4b8fead7
Bill Gates <me@theholytachanka.com>
2024-04-23 18:33:18 +0300

    readded appimage support

--- nixos/common/misc.nix
+++ nixos/common/misc.nix
@@ -58,8 +58,7 @@
     })
     config.nix.registry;
   # Enable appimage support
-  /*
-     boot.binfmt.registrations.appimage = {
+  boot.binfmt.registrations.appimage = {
     wrapInterpreterInShell = false;
     interpreter = "${pkgs.appimage-run}/bin/appimage-run";
     recognitionType = "magic";
@@ -67,7 +66,6 @@
     mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
     magicOrExtension = "\\x7fELF....AI\\x02";
   };
-  */
   environment.variables = {
     RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}"; # Support completion in vscode
   };
