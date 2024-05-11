a4fec54a53213b92d0912a51365075ea3c369afa
Bill Gates <me@theholytachanka.com>
2024-05-11 09:40:23 +0000

    added openGL

--- nixos/mute/default.nix
+++ nixos/mute/default.nix
@@ -61,6 +61,19 @@
     };
   };
   powerManagement.powertop.enable = true;
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override {enableHybridCodec = true;};
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";}; # Force intel-media-driver
   # System state version
   system.stateVersion = "23.05";
 }
