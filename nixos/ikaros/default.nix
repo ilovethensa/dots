ef0de7ce39379d666679f2819ca5cc9220e56b69
Bill Gates <me@theholytachanka.com>
2024-05-13 10:45:05 +0000

    added jellyseerr

--- nixos/ikaros/default.nix
+++ nixos/ikaros/default.nix
@@ -57,7 +57,7 @@
       enable = true;
       dockerCompat = true;
       dockerSocket.enable = true;
-      autoPrune.enable = true;
+      autoPrun/var/lib/jellyseerr/e.enable = true;
     };
   };
 
--- nixos/ikaros/services/jellyseerr.nix
+++ nixos/ikaros/services/jellyseerr.nix
@@ -4,14 +4,8 @@
   config,
   ...
 }: {
-  virtualisation.oci-containers.containers."jellyseerr" = {
-    image = "fallenbagel/jellyseerr:latest";
-    autoStart = true;
-    volumes = [
-      "/srv/data/jellyseerr:/app/config"
-    ];
-    ports = [
-      "5055:5055"
-    ];
-  };
+  #services.jellyseerr.enable = true;
+  environment.persistence."/nix/persist".directories = [
+    "/var/lib/jellyseerr"
+  ];
 }
