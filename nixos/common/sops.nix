a724ba4550f95598dcdfacc0938e0db55d6d2554
Bill Gates <me@theholytachanka.com>
2024-05-13 07:56:32 +0000

    IT WORKS

--- nixos/common/sops.nix
+++ nixos/common/sops.nix
@@ -7,7 +7,7 @@
   sops,
   ...
 }: {
-  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFile = ./../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.sshKeyPaths = ["/home/tht/.ssh/id_ed25519"];
   sops.age.keyFile = "/home/tht/.config/sops/age/keys.txt";
