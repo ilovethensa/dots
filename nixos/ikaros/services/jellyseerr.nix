cf9b0820087ac6c82a68586369e48bd5faddc47c
Bill Gates <me@theholytachanka.com>
2024-05-13 10:54:23 +0000

    back to containers

--- nixos/ikaros/services/jellyseerr.nix
+++ nixos/ikaros/services/jellyseerr.nix
@@ -4,8 +4,14 @@
   config,
   ...
 }: {
-  services.jellyseerr.enable = true;
-  environment.persistence."/nix/persist".directories = [
-    "/var/lib/jellyseerr"
-  ];
+  virtualisation.oci-containers.containers."jellyseerr" = {
+    image = "fallenbagel/jellyseerr:latest";
+    autoStart = true;
+    volumes = [
+      "/srv/data/jellyseerr:/app/config"
+    ];
+    ports = [
+      "5055:5055"
+    ];
+  };
 }
