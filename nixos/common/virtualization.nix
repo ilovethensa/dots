f5d484a17582a0bc78179594b08e07be2e2e7713
Bill Gates <me@theholytachanka.com>
2024-05-13 15:15:16 +0000

    fix

--- nixos/common/virtualization.nix
+++ nixos/common/virtualization.nix
@@ -9,14 +9,12 @@
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
   virtualisation = {
-    virtualisation = {
-      oci-containers.backend = "podman";
-      podman = {
-        enable = true;
-        dockerCompat = true;
-        dockerSocket.enable = true;
-        autoPrune.enable = true;
-      };
+    oci-containers.backend = "podman";
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      dockerSocket.enable = true;
+      autoPrune.enable = true;
     };
   };
   environment.systemPackages = with pkgs; [
