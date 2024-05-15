c848a7124071e21c13104b3ae9e0e93c3b504523
Bill Gates <me@theholytachanka.com>
2024-05-15 14:17:18 +0000

    added glance, now nixified

--- nixos/ikaros/services/glance.nix
+++ nixos/ikaros/services/glance.nix
@@ -3,56 +3,82 @@
   lib,
   config,
   ...
-}: {
-  environment.etc."glance.yml".text = ''
-    pages:
-    - name: Home
-      columns:
-        - size: small
-          widgets:
-            - type: calendar
+}: let
+  settings = {
+    pages = [
+      {
+        name = "Home";
+        columns = [
+          {
+            size = "small";
+            widgets = [
+              {
+                type = "calendar";
+              }
+              {
+                type = "rss";
+                limit = 10;
+                collapse-after = 3;
+                cache = "3h";
+                feeds = [
+                  {url = "https://ciechanow.ski/atom.xml";}
+                  {
+                    url = "https://www.joshwcomeau.com/rss.xml";
+                    title = "Josh Comeau";
+                  }
+                  {url = "https://samwho.dev/rss.xml";}
+                  {url = "https://awesomekling.github.io/feed.xml";}
+                  {
+                    url = "https://ishadeed.com/feed.xml";
+                    title = "Ahmad Shadeed";
+                  }
+                ];
+              }
+            ];
+          }
+          {
+            size = "full";
+            widgets = [
+              {type = "hacker-news";}
+              {
+                type = "videos";
+                channels = [
+                  "UCR-DXc1voovS8nhAvccRZhg" # Jeff Geerling
+                  "UCv6J_jJa8GJqFwQNgNrMuww" # ServeTheHome
+                  "UCOk-gHyjcWZNj3Br4oxwh0A" # Techno Tim
+                ];
+              }
+              {
+                type = "reddit";
+                subreddit = "selfhosted";
+              }
+            ];
+          }
+          {
+            size = "small";
+            widgets = [
+              {
+                type = "weather";
+                location = config.sops.secrets."location";
+              }
+            ];
+          }
+        ];
+      }
+    ];
+  };
 
-            - type: rss
-              limit: 10
-              collapse-after: 3
-              cache: 3h
-              feeds:
-                - url: https://ciechanow.ski/atom.xml
-                - url: https://www.joshwcomeau.com/rss.xml
-                  title: Josh Comeau
-                - url: https://samwho.dev/rss.xml
-                - url: https://awesomekling.github.io/feed.xml
-                - url: https://ishadeed.com/feed.xml
-                  title: Ahmad Shadeed
-
-        - size: full
-          widgets:
-            - type: hacker-news
-
-            - type: videos
-              channels:
-                - UCR-DXc1voovS8nhAvccRZhg # Jeff Geerling
-                - UCv6J_jJa8GJqFwQNgNrMuww # ServeTheHome
-                - UCOk-gHyjcWZNj3Br4oxwh0A # Techno Tim
-
-            - type: reddit
-              subreddit: selfhosted
-
-        - size: small
-          widgets:
-            - type: weather
-              location: Kochevo, Bulgaria
-
-  '';
+  settingsFormat = pkgs.formats.yaml {};
+in {
+  sops.secrets.location = {};
   virtualisation.oci-containers.containers."glance" = {
     image = "glanceapp/glance";
     ports = ["8080:8080"];
     volumes = [
-      "/etc/glance.yml:/app/glance.yml"
+      "${settingsFormat.generate "glance.yaml" settings}:/app/glance.yml"
       #"/etc/timezone:/etc/timezone:ro"
       #"/etc/localtime:/etc/localtime:ro"
     ];
     autoStart = true;
-    extraOptions = ["--pid=host"];
   };
 }
