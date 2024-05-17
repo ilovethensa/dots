05c9a22de399cc7fa20038325bf6dea71b628b62
Bill Gates <me@theholytachanka.com>
2024-05-17 12:17:16 +0300

    pls work

--- hosts/ikaros/default.nix
+++ hosts/ikaros/default.nix
@@ -17,6 +17,7 @@
     ./services/jellyseerr.nix
     ./services/glance.nix
     ./services/cron.nix
+    ./services/wireguard.nix
     inputs.home-manager.nixosModules.home-manager
     inputs.sops-nix.nixosModules.sops
   ];
