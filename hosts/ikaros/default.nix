3e806950085d8ce1709e6583a260e5b196616e3a
Bill Gates <me@theholytachanka.com>
2024-05-17 10:50:03 +0300

    forgot to import(oops)

--- hosts/ikaros/default.nix
+++ hosts/ikaros/default.nix
@@ -17,6 +17,7 @@
     ./services/jellyseerr.nix
     ./services/glance.nix
     ./services/cron.nix
+    ./service/wireguard.nix
     inputs.home-manager.nixosModules.home-manager
     inputs.sops-nix.nixosModules.sops
   ];
