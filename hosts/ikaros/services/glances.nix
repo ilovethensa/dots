e251c5379f8a27d455b991fe5997877a36a3b19a
Bill Gates <me@theholytachanka.com>
2024-05-24 18:37:38 +0300

    added glances

---
+++ hosts/ikaros/services/glances.nix
@@ -0,0 +1,19 @@
+{...}: {
+  virtualisation.oci-containers.containers."glances" = {
+    image = "nicolargo/glances:latest-full";
+    autoStart = true;
+    ports = [
+      "61208-61209:61208-61209"
+    ];
+    volumes = [
+      "/run/user/1000/podman/podman.sock:/run/user/1000/podman/podman.sock:ro"
+      "/var/run/docker.sock:/var/run/docker.sock:ro"
+    ];
+    environment = {
+      GLANCES_OPT = "-w";
+    };
+    extraOptions = [
+      "--pid host"
+    ];
+  };
+}
