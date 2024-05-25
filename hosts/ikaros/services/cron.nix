19e4fed3650416113e83de5105c7a865084e0ec1
Bill Gates <me@theholytachanka.com>
2024-05-25 08:29:53 +0300

    added a space check script

--- hosts/ikaros/services/cron.nix
+++ hosts/ikaros/services/cron.nix
@@ -1,4 +1,16 @@
-{pkgs, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
+  check-space = pkgs.writeShellScriptBin "check-space.sh" ''
+    USER_KEY="${config.sops.secrets.pushover_user}"
+    API_TOKEN="${config.sops.secrets.pushover_key}"
+    for dir in /nix /srv/nvme /srv/Media; do
+      [ $(df "$dir" --output=pcent | tail -1 | tr -d ' %') -ge 90 ] && \
+      ${pkgs.curl}/bin/curl -s -F "token=$API_TOKEN" -F "user=$USER_KEY" -F "message=Warning: $dir is full!" https://api.pushover.net/1/messages.json
+    done
+  '';
   restart-podman = pkgs.writeShellScriptBin "restart-podman.sh" ''
     systemctl list-units | grep -o 'podman-[a-zA-Z0-9\-]*.service' | sort | uniq | while read -r unit; do
       echo "Restarting $unit"
@@ -6,13 +18,17 @@
     done
   '';
 in {
+  sops.secrets.pushover_user = {};
+  sops.secrets.pushover_key = {};
   environment.systemPackages = [
     restart-podman
+    check-space
   ];
   services.cron = {
     enable = true;
     systemCronJobs = [
       "0 * /4 * * * restart-podman.sh"
+      "*/5 * * * * check-space.sh"
     ];
   };
 }
