ba72b0aaf0c84cfcfc68cbf83075b8dc88e64a2a
Bill Gates <me@theholytachanka.com>
2024-05-26 19:53:37 +0300

    work

--- hosts/ikaros/services/homepage.nix
+++ hosts/ikaros/services/homepage.nix
@@ -43,18 +43,16 @@
           style = "row";
           columns = 2;
         };
+        Downloads = {
+          header = false;
+          style = "row";
+          columns = 2;
+        };
         Media = {
           header = false;
           style = "row";
           columns = 3;
         };
-        /*
-           Downloads = {
-          header = false;
-          style = "row";
-          columns = 2;
-        };
-        */
       };
     };
 
