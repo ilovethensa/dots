1470c6806eab4923ff93c1c62c94e3827705db1a
Bill Gates <me@theholytachanka.com>
2024-05-27 20:42:08 +0300

    made kasm be a container

--- hosts/ikaros/services/kasm.nix
+++ hosts/ikaros/services/kasm.nix
@@ -1,7 +1,20 @@
 {...}: {
-  services.kasmweb = {
-    enable = true;
-    datastorePath = "/mnt/data/kasm";
-    listenPort = 5412;
+  virtualisation.oci-containers.containers."kasm" = {
+    image = "lscr.io/linuxserver/kasm:latest";
+    autoStart = true;
+    volumes = [
+      "/mnt/data/kasm/data:/opt"
+      "/mnt/data/kasm/profiles:/profiles"
+      "/dev/input:/dev/input"
+      "/run/udev/data:/run/udev/data"
+    ];
+    environment = {
+      KASM_PORT = 443;
+    };
+    ports = [
+      "3000:3000"
+      "443:443"
+    ];
+    extraOptions = ["--privileged"];
   };
 }
