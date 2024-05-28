8d8804d7b6d68ff3cabf0dd4469e0c2f4008e90d
Bill Gates <me@theholytachanka.com>
2024-05-28 16:56:08 +0300

    moved to another port

--- hosts/ikaros/services/changedetection.nix
+++ hosts/ikaros/services/changedetection.nix
@@ -2,7 +2,7 @@
   services.changedetection-io = {
     enable = true;
     datastorePath = "/mnt/data/changedetection";
-    port = 3000;
+    port = 5603;
     playwrightSupport = true;
     listenAddress = "0.0.0.0";
   };
