a131e7453ea8707af7f02a8d132a58a26ee79fa0
Bill Gates <me@theholytachanka.com>
2024-05-28 16:46:33 +0300

    JUST FUCKING WORK

--- hosts/ikaros/services/changedetection.nix
+++ hosts/ikaros/services/changedetection.nix
@@ -1,32 +1,8 @@
 {...}: {
-  virtualisation.oci-containers.containers = {
-    "changedetection" = {
-      image = "dgtlmoon/changedetection.io";
-      autoStart = true;
-      environment = {
-        PLAYWRIGHT_DRIVER_URL = "ws://192.168.1.111:3000";
-      };
-      volumes = [
-        "/mnt/data/changedetection:/datastore"
-      ];
-      ports = [
-        "5000:5000"
-      ];
-    };
-    "playwright-chrome" = {
-      image = "dgtlmoon/sockpuppetbrowser:latest";
-      extraOptions = [
-        "--cap-add=SYS_ADMIN"
-      ];
-      environment = {
-        SCREEN_WIDTH = "1920";
-        SCREEN_HEIGHT = "1024";
-        SCREEN_DEPTH = "16";
-        MAX_CONCURRENT_CHROME_PROCESSES = "10";
-      };
-      ports = [
-        "3000:3000"
-      ];
-    };
+  changedetection-io = {
+    enable = true;
+    datastorePath = "/mnt/data/changedetection";
+    port = 3000;
+    playwrightSupport = true;
   };
 }
