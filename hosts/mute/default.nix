c0073d7889ac398725f493891bd303a1ef212353
Bill Gates <me@theholytachanka.com>
2024-05-30 19:32:10 +0300

    added plymouth

--- hosts/mute/default.nix
+++ hosts/mute/default.nix
@@ -52,6 +52,35 @@
     virtualization = true;
     gnome = true;
   };
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "lone";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["lone"];
+        })
+      ];
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+  };
   # System state version
   system.stateVersion = "23.05";
 }
