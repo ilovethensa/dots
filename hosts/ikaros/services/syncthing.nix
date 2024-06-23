dd700dc3e28e66893970e471e66149e18dc7ab4e
Bill Gates <me@theholytachanka.com>
2024-06-23 22:33:50 +0300

    fix

--- hosts/ikaros/services/syncthing.nix
+++ hosts/ikaros/services/syncthing.nix
@@ -4,5 +4,9 @@
     guiAddress = "0.0.0.0:8384";
     dataDir = "/mnt/media/sync";
     configDir = "/mnt/data/syncthing";
+    openDefaultPorts = true;
   };
+  networking.firewall.allowedTCPPorts = [
+    8384
+  ];
 }
