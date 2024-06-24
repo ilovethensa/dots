4cdb9e830a164fdba1d5ee3c34e72b7f0f72b3b1
Bill Gates <me@theholytachanka.com>
2024-06-24 14:42:55 +0300

    switched to nginx

--- hosts/ikaros/services/caddy.nix
+++ hosts/ikaros/services/caddy.nix
@@ -1,45 +1,56 @@
 {config, ...}: {
+  # Secrets
   sops.secrets.cloudflare_key = {};
   sops.templates."acme_env".content = ''
     CLOUDFLARE_DNS_API_TOKEN="${config.sops.placeholder.cloudflare_key}"
   '';
-  services.caddy = {
+
+  # Nginx service configuration
+  services.nginx = {
     enable = true;
     virtualHosts = {
-      "mc.theholytachanka.com".extraConfig = ''
-        reverse_proxy 192.168.1.111:25565
-      '';
-      "vpn.theholytachanka.com".extraConfig = ''
-        reverse_proxy 192.168.1.111:51821
-      '';
-      "mindustry.theholytachanka.com".extraConfig = ''
-        reverse_proxy 192.168.1.111:6567
-      '';
-      "cloud.local".extraConfig = ''
-        reverse_proxy 192.168.1.111:80
-        tls internal
-      '';
+      "mc.theholytachanka.com" = {
+        locations = {
+          "/" = {
+            proxyPass = "http://192.168.1.111:25565";
+          };
+        };
+      };
+      "vpn.theholytachanka.com" = {
+        locations = {
+          "/" = {
+            proxyPass = "http://192.168.1.111:51821";
+          };
+        };
+      };
+      "mindustry.theholytachanka.com" = {
+        locations = {
+          "/" = {
+            proxyPass = "http://192.168.1.111:6567";
+          };
+        };
+      };
+      "cloud.local" = {
+        locations = {
+          "/" = {
+            proxyPass = "http://192.168.1.111:80";
+          };
+        };
+        enableACME = false;
+        forceSSL = false;
+      };
     };
   };
+
+  # Cloudflare Dynamic DNS Update Service
   services.cfdyndns = {
     enable = true;
-    apiTokenFile = config.sops.secrets."cloudflare_key".path;
+    apiTokenFile = config.sops.secrets.cloudflare_key.path;
     records = [
       "mc.theholytachanka.com"
       "vpn.theholytachanka.com"
       "mindustry.theholytachanka.com"
     ];
-  };
-  security.acme = {
-    acceptTerms = true;
-    defaults = {
-      email = "me@theholytachanka.com";
-      dnsProvider = "cloudflare";
-      environmentFile = config.sops.templates.acme_env.path;
-    };
-    certs = {
-      ${config.services.nextcloud.hostName}.email = "me@theholytachanka.com";
-    };
   };
   networking.firewall.allowedTCPPorts = [
     443
