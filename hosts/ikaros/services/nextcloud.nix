fe17db0be813e735c23cdce010d85727d8d6b35f
Bill Gates <me@theholytachanka.com>
2024-06-24 15:52:12 +0300

    work pls

--- hosts/ikaros/services/nextcloud.nix
+++ hosts/ikaros/services/nextcloud.nix
@@ -29,7 +29,7 @@
     ensureUsers = [
       {
         name = "nextcloud";
-        ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
+        ensureDBOwnership = true;
       }
     ];
   };
