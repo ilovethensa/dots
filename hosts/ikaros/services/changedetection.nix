a63770db8c76c88dd58f48c819ba37384f831e35
Bill Gates <me@theholytachanka.com>
2024-06-25 08:00:00 +0300

    pls work

--- hosts/ikaros/services/changedetection.nix
+++ hosts/ikaros/services/changedetection.nix
@@ -1,14 +1,44 @@
 {...}: {
-  services.changedetection-io = {
-    enable = true;
-    # webDriverSupport = true;
-    playwrightSupport = true;
-    datastorePath = "/mnt/data/changedetection";
-    port = 4361;
-    listenAddress = "192.168.1.111";
+  virtualisation.oci-containers.containers = {
+    changedetection-io-playwright = {
+      image = "browserless/chrome";
+      environment = {
+        SCREEN_WIDTH = "1920";
+        SCREEN_HEIGHT = "1024";
+        SCREEN_DEPTH = "16";
+        ENABLE_DEBUGGER = "false";
+        PREBOOT_CHROME = "true";
+        CONNECTION_TIMEOUT = "300000";
+        MAX_CONCURRENT_SESSIONS = "10";
+        CHROME_REFRESH_TIME = "600000";
+        DEFAULT_BLOCK_ADS = "true";
+        DEFAULT_STEALTH = "true";
+      };
+      ports = [
+        "127.0.0.1:4444:3000"
+      ];
+      extraOptions = ["--network=bridge"];
+    };
+    changedetection-io = {
+      image = "ghcr.io/dgtlmoon/changedetection.io:0.45.24";
+      environment = {
+        PLAYWRIGHT_DRIVER_URL = "ws://192.168.1.111:3000";
+      };
+      ports = [
+        "5000:5000"
+      ];
+      extraOptions = ["--network=bridge"];
+      volumes = [
+        "/mnt/data/changedetection:/datastore"
+      ];
+      dependsOn = [
+        "changedetection-io-playwright"
+      ];
+    };
   };
+
   networking.firewall.allowedTCPPorts = [
-    4361
+    5000
     4444
   ];
 }
