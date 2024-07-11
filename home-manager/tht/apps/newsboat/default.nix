563cfcfdb3963c46653c327e7f1272fe25b7219d
Bill Gates <me@theholytachanka.com>
2024-07-11 13:15:29 +0300

    changed pass

--- home-manager/tht/apps/newsboat/default.nix
+++ home-manager/tht/apps/newsboat/default.nix
@@ -1,12 +1,12 @@
 {config, ...}: {
-  sops.secrets.vpn_pass = {};
+  sops.secrets.rss_pass = {};
   programs.newsboat = {
     enable = true;
     extraConfig = ''
       urls-source "freshrss"
       freshrss-url "https://rss.zdx.fr/api/greader.php"
       freshrss-login "tht"
-      freshrss-passwordfile ${config.sops.secrets.vpn_pass.path}
+      freshrss-passwordfile ${config.sops.secrets.rss_pass.path}
     '';
   };
 }
