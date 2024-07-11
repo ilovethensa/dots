b0606be520603573680c74127787613920df5fcc
Bill Gates <me@theholytachanka.com>
2024-07-11 13:27:55 +0300

    im so dumb

--- home-manager/tht/apps/newsboat/default.nix
+++ home-manager/tht/apps/newsboat/default.nix
@@ -5,7 +5,7 @@
     extraConfig = ''
       urls-source "freshrss"
       freshrss-url "https://rss.zdx.fr/api/greader.php"
-      freshrss-login "tht"
+      freshrss-login "TheHolyTachanka"
       freshrss-passwordfile ${config.sops.secrets.rss_pass.path}
     '';
   };
