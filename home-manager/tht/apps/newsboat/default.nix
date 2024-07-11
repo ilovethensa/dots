f44730558407e7061864f873937b9e6320a395e7
Bill Gates <me@theholytachanka.com>
2024-07-11 13:01:45 +0300

    fix

--- home-manager/tht/apps/newsboat/default.nix
+++ home-manager/tht/apps/newsboat/default.nix
@@ -6,7 +6,7 @@
       urls-source "freshrss"
       freshrss-url "https://rss.zdx.fr/api/greader.php"
       freshrss-login "tht"
-      freshrss-passwordfile ${config.secrets.vpn_pass.path}
+      freshrss-passwordfile ${config.sops.secrets.vpn_pass.path}
     '';
   };
 }
