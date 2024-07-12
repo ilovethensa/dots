ef4613960051d226c4b8d3b03a0d03fd20d269db
Bill Gates <me@theholytachanka.com>
2024-07-12 14:16:49 +0300

    added unmanic

---
+++ hosts/ikaros/services/unmanic.nix
@@ -0,0 +1,16 @@
+{...}: {
+  virtualisation.oci-containers.containers."unmanic" = {
+    image = "josh5/unmanic";
+    ports = [
+      "8888:8888"
+    ];
+    volumes = [
+      "/mnt/data/unmanic/config:/config"
+      "/mnt/media/library:/library"
+      "/mnt/data/unmanic/cache:/tmp/unmanic"
+    ];
+    extraOptions = [
+      "--device=/dev/dri"
+    ];
+  };
+}
