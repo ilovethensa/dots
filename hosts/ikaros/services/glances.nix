ec4ad857087d76619bc68846b6b367e2e7f2d837
ilovethensa <ilovethensa@users.noreply.github.com>
2024-12-31 03:19:28 +0000

    Updated flake and containers

--- hosts/ikaros/services/glances.nix
+++ hosts/ikaros/services/glances.nix
@@ -1,6 +1,6 @@
 {...}: {
   virtualisation.oci-containers.containers."glances" = {
-    image = "nicolargo/glances:ubuntu-4.3.0.5-full";
+    image = "nicolargo/glances:ubuntu-4.3.0.6-full";
     autoStart = true;
     ports = [
       "61208-61209:61208-61209"
