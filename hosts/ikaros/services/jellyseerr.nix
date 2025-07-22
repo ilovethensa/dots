5f9e87583c159ca2060fced77d3a3f02e9c91b29
ilovethensa <ilovethensa@users.noreply.github.com>
2025-07-22 03:58:52 +0000

    Updated flake and containers

--- hosts/ikaros/services/jellyseerr.nix
+++ hosts/ikaros/services/jellyseerr.nix
@@ -1,6 +1,6 @@
 {...}: {
   virtualisation.oci-containers.containers."jellyseerr" = {
-    image = "fallenbagel/jellyseerr:2.7.1";
+    image = "fallenbagel/jellyseerr:2.7.2";
     autoStart = true;
     volumes = [
       "/mnt/data/jellyseerr:/app/config"
